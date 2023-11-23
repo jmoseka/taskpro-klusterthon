fb785bb924856b35dcff5f30a83cdbfa675047b2
jmoseka <jamylam3@gmail.com>
2023-11-23 19:28:34 +0300

    add client component, create forms style and everything

--- src/components/Manage/Manage.js
+++ src/components/Manage/Manage.js
@@ -1,16 +1,25 @@
 import MessageBoard from "../MessageBoard/MessageBoard";
-import add from '../../icons/0.75x/pluswhite.png';
 
 import './Manage.css';
 
 import { useState } from "react";
-import ClientList from "./ClientList/ClientList";
+import AddClient from "./AddClient/AddClient";
+import ManageClient from "./ManageClient/ManageClient";
 
 function Manage() {
-    const [activeTab, setActiveTab] = useState('1');
 
-    const handleTabClick = (index) => {
-        setActiveTab(index)
+    const [openClient, setOpenClient] = useState(false)
+
+    const handleOpenClient = (selection) => {
+        setOpenClient(selection);
+    }
+
+    const handleCloseClient = (selection) => {
+        setOpenClient(selection);
+    }
+
+    const handleSaveClient = (selection) => {
+        setOpenClient(selection)
     }
 
 
@@ -18,28 +27,12 @@ function Manage() {
     return (
         <div className="h-fit flex flex-col gap-5 ">
             <MessageBoard />
-
-            <div className="card py-3">
-                <div className=" px-7 flex justify-between items-center">
-                    <p className="font-medium">Manage</p>
-                    <button type="button" className=" bg-yellow rounded-lg 
-                     font-medium text-icon-container gap-1 p-2">
-                        <span><img src={add} alt="add client" /></span>
-                        <span className="capitalize text-sm text-white">New client</span>
-                    </button>
-                </div>
-
-                <div className="tab font-normal text-sm w-full  border-b-[1px] border-b-grey ">
-                    <div className="px-7 flex text-center gap-7">
-                        <button index={1} onClick={() => handleTabClick('1')} type="button" className={`tab-buttons ${activeTab === '1' ? 'border-b-green' : ' border-b-transparent'}`}>Clients</button>
-                        <button index={2} onClick={() => handleTabClick('2')} type="button" className={`tab-buttons ${activeTab === '2' ? 'border-b-green' : ' border-b-transparent'}`}>Tasks</button>
-                        <button index={3} onClick={() => handleTabClick('3')} type="button" className={`tab-buttons ${activeTab === '3' ? 'border-b-green' : ' border-b-transparent'} `}>Roles</button>
-                    </div>
-                </div>
-
-                <ClientList />
-
-            </div>
+            {
+                openClient === true ?
+                    <AddClient  onCloseClient={handleCloseClient} onSaveClient={handleSaveClient}  />
+                    :
+                    <ManageClient onAddClient={handleOpenClient} />
+            }
 
 
         </div>
