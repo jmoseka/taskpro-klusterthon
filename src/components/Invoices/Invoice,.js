8856cf7a8b9b8f60963a46dbc413fc9c3411b79d
jmoseka <jamylam3@gmail.com>
2023-11-23 19:59:29 +0300

    add functional props for open and closing invoices screens

--- src/components/Invoices/Invoice,.js
+++ src/components/Invoices/Invoice,.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
 
 function Invoice() {
+    const [openInvoice, setOpenInvoice] = useState(false)
+
+    const handleOpenInvoice = (selection) => {
+        setOpenInvoice(selection);
+    }
+
+    const handleCloseInvoice = (selection) => {
+        setOpenInvoice(selection);
+    }
+
+
     return (
         <div className="">
-            <h1 className='text-3xl text-red'>Invoice</h1>
+            
         </div>
     );
 }
