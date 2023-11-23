fd6af477f2a7071ca693e504363427b05411a6b2
jmoseka <jamylam3@gmail.com>
2023-11-23 19:38:10 +0300

    switch tab style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import Mainbar from "../Mainbar/Mainbar";
 import Sidebar from "../Sidebar/Sidebar";
 
 function Home() {
-  const [selectedMenu, setSelectedMenu] = useState('dashboard'); 
+  const [selectedMenu, setSelectedMenu] = useState('invoice');
 
   const handleMenuClick = (menuItem) => {
     setSelectedMenu(menuItem);
@@ -13,7 +13,7 @@ function Home() {
   return (
     <div className="mx-auto flex max-h-[1117px] h-[100vh] max-w-[1728px] pb-1 ">
       <Sidebar onMenuClick={handleMenuClick} />
-      <Mainbar selectedMenu={selectedMenu}/>
+      <Mainbar selectedMenu={selectedMenu} />
     </div>
   );
 }
