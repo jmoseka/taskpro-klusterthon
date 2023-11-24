3945ec082bb7809fc0f406049d12e9388c5d2dbf
oladee <deemajor230600@gmail.com>
2023-11-25 00:34:44 +0100

    almost done with forms

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignIn from './pages/Forms/SignIn';
+import SignUp from './pages/Forms/SignUp';
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App font-poppins">
-      <SignIn/>
-     {/* <Dashboard /> */}
+    <div className="App font-poppins pb-11">
+      <Routes>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+      </Routes>
     </div>
   );
 }
--- src/pages/Forms/SignIn.jsx
+++ src/pages/Forms/SignIn.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { getAuth, signInWithEmailAndPassword,GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {app} from '../../firebase'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SignIn = () => {
 
@@ -13,11 +17,14 @@ const SignIn = () => {
       email: Yup.string().email("Invalid Email Address!!").required("Can't Leave Empty"),
       password: Yup.string().required("Can't Leave Empty")
     }),
-    onSubmit: (values, e)=>{
-      e.preventDefault()
-      formik.handleSubmit()
+    onSubmit: (values)=>{
+      const verify = async()=> {
+        
+      }
+      verify()
     }
   })
+  
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col  w-[85%] max-w-[500px]">
@@ -30,10 +37,13 @@ const SignIn = () => {
         </header>
         <main className="mt-16">
           <h2 className="text-3xl font-semibold mb-6">Sign In To Biz Invoice</h2>
-          <form action="" className="text-black p-8 border-2 border-[#72D1CC] "
-            onSubmit={formik.submitForm}
+          <form action="" className="text-black signin p-8 border-2 border-[#72D1CC] "
+            onSubmit={(e) => {
+              e.preventDefault()
+              formik.handleSubmit()
+            }}
           >
-            <div className="flex items-center bg-white p-2 rounded-xl cursor-pointer">
+            <div className="flex items-center bg-white p-2 rounded-xl cursor-pointer input">
               <img src="assets/googleIcon.png" alt="Google Logo" />
               <div className="w-[90%]">
                 <h4 className="text-xl md:text-2xl font-medium">Sign In With Google</h4>
@@ -44,10 +54,9 @@ const SignIn = () => {
               <p className="text-base md:text-lg">or with your email below</p>
               <hr className="w-[25%] max-w-[150px]" />
             </div>
-            <div className="flex items-center bg-white gap-3 p-2">
+            <div className="flex items-center bg-white gap-3 p-2 input">
               <img src="assets/WorkEmailIcon.png" alt="Email Icon" />
               <input
-
                 name="email"
                 type="email"
                 placeholder="Work Email"
@@ -59,11 +68,10 @@ const SignIn = () => {
             </div>
             {formik.touched.email && formik.errors.email ? (
               <div className="text-red text-start font-medium italic">{formik.errors.email}</div>
-            ) : null}
-            <div className="flex items-center bg-white gap-3 p-2 my-4">
+            ) : formik.errors.password = ""}
+            <div className="flex items-center bg-white gap-3 p-2 my-4 input">
               <img src="assets/passwordIcon.png" alt="password" />
               <input
-
                 name="password"
                 type="password"
                 placeholder="password"
@@ -78,6 +86,7 @@ const SignIn = () => {
             ) : null}
             <button className="bg-veryGreen w-full text-lg text-white py-3 font-medium rounded-xl">Sign In</button>
           </form>
+          <ToastContainer/>
           <div>
             
           </div>
