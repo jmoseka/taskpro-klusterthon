4f67af3956f2d5ae33e572ce6bae3dd5cb9fbd42
jmoseka <jamylam3@gmail.com>
2023-11-27 16:01:27 +0300

    improved logout button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,26 +2,21 @@ import { useEffect, useState } from "react";
 import Mainbar from "../Mainbar/Mainbar";
 import Sidebar from "../Sidebar/Sidebar";
 import { useNavigate } from "react-router";
-import logo from '../../icons/2x/logo.png'
 
 function Home() {
 
   let navigate = useNavigate()
-  const [openScreen, setOpenScreen] = useState(true)
-
-
 
   useEffect(() => {
     let bizToken = localStorage.getItem('bizToken')
     if (bizToken) {
-      setOpenScreen(false)
       navigate('/dashboard')
     }
     else if (!bizToken) {
       navigate('/signin')
     }
 
-  },[navigate])
+  }, [navigate])
 
   const [selectedMenu, setSelectedMenu] = useState('dashboard');
 
@@ -31,24 +26,13 @@ function Home() {
 
 
   return (
-    <div className="">
-      {
-        openScreen ?
-          <div className="h-[100vh] w-[100vh] bg-veryGreen flex justify-center items-center">
-            <img src={logo} alt="bizhub logo"/>
 
-          </div>
-
-          :
-
-          <div className="relative mx-auto flex h-[100vh] ">
-            <Sidebar onMenuClick={handleMenuClick} />
-            <Mainbar selectedMenu={selectedMenu} />
-          </div>
-
-      }
-
+    <div className="relative mx-auto flex h-[100vh] ">
+      <Sidebar onMenuClick={handleMenuClick} />
+      <Mainbar selectedMenu={selectedMenu} />
     </div>
+
+
   );
 }
 
