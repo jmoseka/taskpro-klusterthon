00ea3c98da81904922e61823b5c9faad51f1932f
jmoseka <jamylam3@gmail.com>
2023-11-30 16:15:18 +0300

    validate form input for create client

--- src/components/Manage/AddClient/AddClient.js
+++ src/components/Manage/AddClient/AddClient.js
@@ -16,24 +16,16 @@ const AddClient = ({ editClientId, editStatus, editClientName, editClientEmail, 
     const [clientAddress, setClientAddress] = useState()
     const [clientTask, setClientTask] = useState()
     const [clientContact, setClientContact] = useState()
+    const initialValues = { client: '', email: '', address: '', task: '', contact: null }
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+    const [overlay, setOverlay] = useState(false);
+    const [notice, setNotice] = useState('')
 
-    const handleInputChangeName = (e) => {
-        setClientName(e.target.value)
-    }
-    const handleInputChangeEmail = (e) => {
-        setClientEmail(e.target.value);
-    }
-    const handleInputChangeAddress = (e) => {
-        setClientAddress(e.target.value);
-    }
-    const handleInputChangeTask = (e) => {
-        setClientTask(e.target.value);
-    }
-    const handleInputChangeContact = (e) => {
-        setClientContact(e.target.value);
-    }
 
     useEffect(() => {
+
         if (editStatus === 'edit') {
             setLoadingAnime(true)
             setTimeout(() => {
@@ -47,7 +39,7 @@ const AddClient = ({ editClientId, editStatus, editClientName, editClientEmail, 
 
         }
 
-    }, [editStatus, setClientName, editClientName, editClientEmail, setClientEmail, editClientContact, setClientContact, editClientAddress, setClientAddress, editClientTask, setClientTask]); // Add dependencies as needed
+    }, [editStatus, setClientName, editClientName, editClientEmail, setClientEmail, editClientContact, setClientContact, editClientAddress, setClientAddress, editClientTask, setClientTask, formErrors, isSubmit, formValues]); // Add dependencies as needed
 
 
 
@@ -57,32 +49,108 @@ const AddClient = ({ editClientId, editStatus, editClientName, editClientEmail, 
             Authorization: `Token ${GetToken()}`,
         };
 
-        const postData = {
-            name: clientName,
-            email: clientEmail,
-            address: clientAddress,
-            task_details: clientTask,
-            contact: clientContact
-        };
-        setLoadingAnime(!loadMessage)
 
-        axios.post('https://bizhub-8955b30ff7e1.herokuapp.com/client/create/', postData, { headers })
-            .then(response => {
-                setLoadMessage(!loadMessage)
-                setLoadingAnime(false);
-                setTimeout(() => {
-                    onSaveClient(false)
-                }, 2000);
-                return response.data;
-            })
-            .catch(error => {
-                return error;
-            });
-            
+        setFormErrors(validate(formValues))
+        if (formValues.client && formValues.email && formValues.address && formValues.task && formValues.contact) {
+            setOverlay(true)
+            setLoadingAnime(true)
+
+            const postData = {
+                name: formValues.client,
+                email: formValues.email,
+                address: formValues.address,
+                task_details: formValues.task,
+                contact: formValues.contact
+            };
+            setLoadingAnime(!loadMessage)
+
+            axios.post('https://bizhub-8955b30ff7e1.herokuapp.com/client/create/', postData, { headers })
+                .then(response => {
+                    setNotice('Client created successfully!')
+                    setLoadingAnime(false);
+                    setTimeout(() => {
+
+                        setOverlay(false)
+                        onSaveClient(false)
+                    }, 3000);
+                    return response.data;
+                })
+                .catch(error => {
+                    setNotice('Please try again!')
+                    setLoadingAnime(false);
+                    setOverlay(false)
+                    return error;
+                });
+
+
+        }
+
+
+        //     name: clientName,
+        //     email: clientEmail,
+        //     address: clientAddress,
+        //     task_details: clientTask,
+        //     contact: clientContact
+        // };
+        // setLoadingAnime(!loadMessage)
+
+        // axios.post('https://bizhub-8955b30ff7e1.herokuapp.com/client/create/', postData, { headers })
+        //     .then(response => {
+        //         setLoadMessage(!loadMessage)
+        //         setLoadingAnime(false);
+        //         setTimeout(() => {
+        //             onSaveClient(false)
+        //         }, 2000);
+        //         return response.data;
+        //     })
+        //     .catch(error => {
+        //         return error;
+        //     });
+
     }
+
+    const handleOnChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value })
+
+
+    }
+
+    const validate = (values) => {
+        const errors = {}
+        const regex = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
+        if (!values.client) {
+            errors.client = 'Name is required'
+        }
+        if (!values.email) {
+            errors.email = 'Email is required'
+        } else if (!regex.test(values.email)) {
+            errors.email = 'Please enter a valid email';
+        }
+
+        if (!values.address) {
+            errors.address = 'Address is required'
+        }
+        if (!values.task) {
+            errors.task = 'Task is required'
+        } else if (values.task.length > 50) {
+            errors.task = 'Please give a short description'
+        }
+
+        if (!values.contact) {
+            errors.contact = 'Contact is required'
+        } else if (values.contact.length > 14) {
+            errors.contact = 'Please enter a valid number'
+        }
+
+        return errors
+    }
+
 
     const handleEditClient = (event) => {
         event.preventDefault();
+        setFormErrors(validate(formValues))
+        setIsSubmit(true);
 
         const headers = {
             Authorization: `Token ${GetToken()}`,
@@ -116,24 +184,31 @@ const AddClient = ({ editClientId, editStatus, editClientName, editClientEmail, 
     return (
         <div className='py-9'>
             <div className="card relative">
-                <div className={`modal-blur ${loadMessage === true ? 'block' : 'hidden'}`}>
-                    <div className='modal-window addmodal '>
-
-                        {
-                            loadingAnime ?
-                                <div className='mx-auto custom-loader'></div>
-
-                                :
-
-                                <span className='flex  flex-col justify-center items-center gap-4'>
-                                    <span>{editStatus === 'edit' ? 'Client updated successfully!' : 'Client created successfully!'}</span>
-                                    <span><img src={success} alt='green check tick box' /></span>
-                                </span>
-                        }
-                    </div>
-                </div>
-
                 {
+                    overlay ?
+                        <div className='modal-blur'>
+                            {
+                                loadingAnime === true ?
+                                    <div className='position-loader'>
+                                        <div class="custom-loader mx-auto"></div>
+                                    </div>
+                                    :
+
+                                    <div className='addmodal'>
+                                        <span className='flex flex-col justify-center items-center gap-4'>
+                                        <span>{editStatus === 'edit' ? notice : notice }</span>
+                                        <span><img src={success} alt='green check tick box' /></span>
+                                    </span>
+                                     </div>
+                                    
+                            }
+
+                        </div>
+                        :
+                        ''
+                }
+
+                {/* {
                     loadingAnimeEdit ?
 
                         <div className='modal-blur'>
@@ -142,7 +217,7 @@ const AddClient = ({ editClientId, editStatus, editClientName, editClientEmail, 
 
 
                         : ''
-                }
+                } */}
 
 
                 <div className='pt-8 pb-4'>
@@ -159,71 +234,86 @@ const AddClient = ({ editClientId, editStatus, editClientName, editClientEmail, 
 
                         <div className='form-control'>
                             <label htmlFor='clientName'>Client name </label>
-                            <input
-                                required
-                                type='text'
-                                id='clientName'
-                                name='clientName'
-                                value={clientName}
-                                onChange={handleInputChangeName}
-                            />
+                            <div className='w-full'>
+                                <input
+                                    type='text'
+                                    id='clientName'
+                                    name='client'
+                                    value={formValues.client}
+                                    onChange={handleOnChange}
+                                />
+
+                                <p className='error-mg'>{formErrors.client}</p>
+                            </div>
 
                         </div>
 
                         <div className='form-control'>
                             <label htmlFor='emailAddress'>Email address </label>
-                            <input
-                                required
-                                type='text'
-                                id='emailAddress'
-                                name='emailAddress'
-                                value={clientEmail}
-                                onChange={handleInputChangeEmail}
+                            <div className='w-full'>
+                                <input
 
-                            />
+                                    type='text'
+                                    id='emailAddress'
+                                    name='email'
+                                    value={formValues.email}
+                                    onChange={handleOnChange}
+
+                                />
+                                <p className='error-mg'>{formErrors.email}</p>
+                            </div>
 
                         </div>
 
                         <div className='form-control'>
                             <label htmlFor='addressClient'>Address </label>
-                            <textarea
-                                required
-                                type='text'
-                                id='addressClient'
-                                name='addressClient'
-                                className="h-[70px] addClientTextarea "
-                                value={clientAddress}
-                                onChange={handleInputChangeAddress}
+                            <div className='w-full'>
+                                <textarea
 
-                            />
+                                    type='text'
+                                    id='addressClient'
+                                    name='address'
+                                    className="h-[70px] addClientTextarea "
+                                    value={formValues.address}
+                                    onChange={handleOnChange}
+
+                                />
+
+                                <p className='error-mg'>{formErrors.address}</p>
+                            </div>
 
                         </div>
 
                         <div className='form-control'>
                             <label htmlFor='taskDetails'>Task details </label>
-                            <textarea
-                                type='text'
-                                id='taskDetails'
-                                name='taskDetails'
-                                value={clientTask}
-                                className="h-[120px] addClientTextarea "
-                                onChange={handleInputChangeTask}
-                            />
+                            <div className='w-full'>
+                                <textarea
+                                    type='text'
+                                    id='taskDetails'
+                                    name='task'
+                                    value={formValues.task}
+                                    className="h-[120px] addClientTextarea "
+                                    onChange={handleOnChange}
+                                />
 
+                                <p className='error-mg'>{formErrors.task}</p>
+                            </div>
                         </div>
 
                         <div className='form-control'>
                             <label htmlFor='emailAddress'>Contact </label>
-                            <input
-                                required
-                                type='number'
-                                id='contactClient'
-                                name='contactClient'
-                                onChange={handleInputChangeContact}
-                                value={clientContact}
+                            <div className='w-full'>
+                                <input
 
-                            />
+                                    type='number'
+                                    id='contactClient'
+                                    name='contact'
+                                    onChange={handleOnChange}
+                                    value={formValues.contact}
 
+                                />
+                                <p className='error-mg'>{formErrors.contact}</p>
+                            </div>
                         </div>
 
                         <div className='form-currency w-full flex items-center'>
